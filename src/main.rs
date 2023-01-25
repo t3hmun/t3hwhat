f18a8fae68a22f08acbc5abc54abe8ac6b890175
t3hmun <git@t3hmun.com>
2023-01-25 21:48:17 +0000

    A function that runs and prints a command.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    exe("lsb_release -a", "Linux disro os version information.");
+}
+
+fn exe(cmd: &str, explanation: &str) {
+    println!("> {explanation}");
+    println!("$ {cmd}");
+    let output = Command::new("sh").arg("-c").arg(cmd).output();
+    let out_vec = match output {
+        Ok(output) => output.stdout,
+        Err(err) => {
+            println!("Execute command failed:\n{err}");
+            return;
+        }
+    };
+
+    let text = String::from_utf8(out_vec);
+    match text {
+        Ok(text) => {
+            println!("{}", text);
+        }
+        Err(err) => {
+            println!("Parse output failed:\n{err}");
+            return;
+        }
+    };
 }
